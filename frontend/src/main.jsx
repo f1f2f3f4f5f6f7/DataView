cceb4cc00170519a0dc2a5d82b2c7efc49a62145
smuke123 <edgarariza6@outlook.com>
2026-03-06 16:22:18 -0500

    Mejoras carga

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PrimeReactProvider } from 'primereact/api'
-
-import 'primereact/resources/themes/lara-light-blue/theme.css'
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
-import 'primeflex/primeflex.css'  
-
+import 'primeflex/primeflex.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
