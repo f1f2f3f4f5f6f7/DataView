b8ee6d817f73dcdd263addade5e03195867e2820
smuke123 <edgarariza6@outlook.com>
2026-03-06 15:13:19 -0500

    Vista de carga

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { PrimeReactProvider } from 'primereact/api'
 import 'primereact/resources/themes/lara-light-blue/theme.css'
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'  
 
 import App from './App.jsx'
 
